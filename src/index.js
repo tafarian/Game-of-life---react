46638a13de041af5798a8234b8d450b43606c2ab
Jakub Wyczesany <ghaelios@gmail.com>
2018-04-10 12:55:22 +0200

    begin method creation

--- src/index.js
+++ src/index.js
@@ -64,6 +64,36 @@ class Main extends React.Component {
         }
     }
 
+    selectBox = (row, col) => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        gridCopy[row][col] = !gridCopy[row][col];
+        this.setState({
+            gridFull: gridCopy
+        });
+    }
+
+    seed = () => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                if (Math.floor(Math.random() * 4) === 1) {
+                    gridCopy[i][j] = true;
+                }
+            }
+        }
+        this.setState({
+            gridFull: gridCopy
+        });
+    }
+
+    playButton = () => {
+        clearInterval(this.intervalId)
+        this.intervalId = setInterval(this.play, this.props.speed);
+    }
+
+    componentDidMount() {
+        this.seed();
+    }
 
     render() {
         return (
@@ -79,7 +109,11 @@ class Main extends React.Component {
             </div>
         );
     }
+}
 
+// function which allows us to deep copy an array
+function arrayClone(arr) {
+    return JSON.parse(JSON.stringify(arr));
 }
 
 ReactDOM.render(<Main />, document.getElementById('root'));
